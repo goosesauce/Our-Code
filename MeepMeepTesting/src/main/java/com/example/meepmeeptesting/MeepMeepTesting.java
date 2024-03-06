330fb68b12c3c1e1bb79254f05340d7f873863da
PetrBytes <peter.shi2010@gmail.com>
2024-03-06 19:18:53 +1300

    2+5 red meepmeep testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,10 +12,44 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
+                .setConstraints(80, 80, Math.toRadians(180), Math.toRadians(180), 18)
                 .followTrajectorySequence(drive ->
+                        //2+5 going on outside route
                         drive.trajectorySequenceBuilder(new Pose2d(-36, -63, Math.toRadians(90)))
-                                .lineToLinearHeading(new Pose2d(-36, -2, Math.toRadians(0)))
+                                .lineToLinearHeading(new Pose2d(-36, -35.5, Math.toRadians(90)))
+                                //place down first pixel
+                                .lineToLinearHeading(new Pose2d(-55, -35.5, Math.toRadians(180)))
+                                .forward(2)
+                                //intake
+                                .lineToLinearHeading(new Pose2d(-36, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(35, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(48, -36, Math.toRadians(180)))
+                                //put on backdrop
+                                .waitSeconds(0.2)
+                                .lineToLinearHeading(new Pose2d(35, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(-36, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(-55, -35.5, Math.toRadians(180)))
+                                .forward(2)
+                                //intake
+                                .lineToLinearHeading(new Pose2d(-36, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(35, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(48, -36, Math.toRadians(180)))
+                                //put on backdrop
+                                .waitSeconds(0.2)
+                                .lineToLinearHeading(new Pose2d(35, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(-36, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(-55, -35.5, Math.toRadians(180)))
+                                .forward(2)
+                                //intake
+                                .lineToLinearHeading(new Pose2d(-36, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(35, -60, Math.toRadians(180)))
+                                .lineToLinearHeading(new Pose2d(48, -36, Math.toRadians(180)))
+                                //put on backdrop
+                                .waitSeconds(0.2)
+                                //2+6
+                                .lineToLinearHeading(new Pose2d(52, -16, Math.toRadians(135)))
+
+
                                 .build()
                 );
 
